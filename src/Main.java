d8607430ddffc4bb09f1cb7e8f361aa017b55b1d
BTHS <sample@bths.edu>
2026-03-04 10:19:14 -0500

    ArrayList Method Madness: Part 2

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList("this", "is", "not", "a", "trivial", "task", "right?"));
-        ArrayListAlgorithms.insertAfterI(stringList, "bob");
+        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList("the", "not", "skim", "pat", "a", "rat", "cat", "sit", "truck", "par", "tar"));
+        ArrayListAlgorithms.removeThree(stringList);
         System.out.println(stringList);
-        ArrayList<String> stringList2 = new ArrayList<String>(Arrays.asList("this", "is", "not", "a", "trivial", "task", "right?"));
-        ArrayListAlgorithms.insertAfterI(stringList2, "hi");
+        ArrayList<String> stringList2 = new ArrayList<String>(Arrays.asList("not", "ton", "lot", "awe", "rat", "art", "tar"));
+        ArrayListAlgorithms.removeThree(stringList2);
         System.out.println(stringList2);
-        ArrayList<String> stringList3 = new ArrayList<String>(Arrays.asList("hi", "lie", "is", "tis"));
-        ArrayListAlgorithms.insertAfterI(stringList3, "mild");
+        ArrayList<String> stringList3 = new ArrayList<String>(Arrays.asList("hi", "pint", "is", "them"));
+        ArrayListAlgorithms.removeThree(stringList3);
         System.out.println(stringList3);
-        ArrayList<String> stringList4 = new ArrayList<String>(Arrays.asList("the", "best", "not", "ever", "seen"));
-        ArrayListAlgorithms.insertAfterI(stringList4, "pie");
-        System.out.println(stringList4);
-
     }
 }
