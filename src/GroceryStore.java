4549cb18e5f5b3ae36a366766a5664123de97d60
BTHS <sample@bths.edu>
2026-03-06 10:25:32 -0500

    Grocery Store

---
+++ src/GroceryStore.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class GroceryStore {
+    /** An array of products normally stocked at the grocery store
+     *  Guaranteed not to be null and to contain only non-null entries
+     */
+    private Product[] productsStocked;
+
+    public GroceryStore(Product[] initialInventory) {
+        productsStocked = initialInventory;
+    }
+
+    /** Returns an ArrayList of indices for Product elements in the
+     *  productsStocked array that need to be reordered, as described in part (a)
+     *  Precondition: min > 0 */
+    public ArrayList<Integer> getReorderList(int min) {
+        ArrayList<Integer> reorder = new ArrayList<>();
+        for (int i = 0; i < productsStocked.length; i++) {
+            if (productsStocked[i].getQuantity() <= min) {
+                reorder.add(i);
+            }
+        }
+        return reorder;
+    }
+
+    /** Returns true if all products named in shoppingList are available for purchase
+     *  and returns false otherwise, as described in part (b)
+     *  Precondition: The products named in shoppingList are found exactly once
+     *  in the productsStocked array.
+     */
+    public boolean checkAvailability(ArrayList<String> shoppingList) {
+        int num = 0;
+        for (String item : shoppingList) {
+            for (Product product : productsStocked) {
+                if (item == product.getName() && product.getQuantity() >= 1) {
+                    num++;
+                }
+            }
+        }
+        return num == shoppingList.size();
+    }
+}
